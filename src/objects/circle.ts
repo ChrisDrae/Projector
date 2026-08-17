d616a0b74b20eb33354ca7eb0127aa3ccb90511c
ChrisDrae <crazylp892@gmail.com>
2026-08-18 00:49:51 +0200

    add concurrent build

--- src/objects/circle.ts
+++ src/objects/circle.ts
@@ -1,13 +1,24 @@
+import { Vec3 } from "../rendering/renderer";
+import { Vec } from "./vectors";
+
 interface CircleProps {
-    radius:  number
+    radius:  number,
+    center: Vec3
 }
 
 class Circle implements CircleProps{
     radius: number;
+    center: Vec3;
 
-    constructor(radius: number){
+    constructor(center: Vec3, radius: number = 0.1){
         this.radius = radius
+        this.center = center
     }
 
-    
+    move(v: Vec3){
+        const tmp = this.center
+        this.center = Vec.addVectors(tmp, v)
+    }
 }
+
+export default Circle
